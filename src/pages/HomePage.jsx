cffeccd8a333391a94423ccf7102411885e6bf8f
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-06-27 22:56:06 +0530

    get stations from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,18 +4,40 @@ import Header from '../components/Header';
 import SearchForm from '../components/SearchForm';
 import Footer from '../components/Footer';
 import trainImage from '../assets/trainImage.png';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const HomePage = () => {
+  const [stations, setStations] = useState([]);
+  console.log(stations);
+  useEffect(() => {
+    const getStations = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/stations');
+        setStations(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getStations();
+  }, []);
+  
   return (
-    <div className="relative max-w-container mx-auto px-4 py-8">
-        <img src={trainImage} alt="Train Background" className="absolute inset-0 w-full h-full object-cover opacity-10 z-0" />
+    <div className="relative px-4 py-8 mx-auto max-w-container">
+        <img src={trainImage} alt="Train Background" className="absolute inset-0 z-0 object-cover w-full h-full opacity-10" />
 
       <div className="home-page">
         <Header />
-        <main className="container mx-auto p-4">
+        <main className="container p-4 mx-auto">
           <SearchForm />
           
-
+          <div>
+            {stations.map((station) => (
+              <div key={station.id}>
+                <h2>{station.name}</h2>
+              </div>
+            ))}
+          </div>
         </main>
         
         <Footer />
