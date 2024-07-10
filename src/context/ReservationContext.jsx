6eb4d14091015c343ecb57cd32dc09ee09c8c9c5
JanakCSE210598B <janaksenevirathne8@gmail.com>
2024-07-10 21:58:06 +0530

    Save the context to local storage.

--- src/context/ReservationContext.jsx
+++ src/context/ReservationContext.jsx
@@ -1,34 +1,43 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 export const ReservationContext = createContext();
 
 export const ReservationProvider = ({ children }) => {
-	const [reservationData, setReservationData] = useState({
-		scheduleId: "",
-		departureStationId: "",
-		fromHaltId: "",
-		toHaltId: "",
-		arrivalStationId: "",
-		departureDate: "",
-		pax: 0,
-		selectedSeats: [],
-		selectedClassId: "",
-		departureTime: "",
-		arrivalTime: "",
-		passengers: [],
-		email: "",
-	});
+  const [reservationData, setReservationData] = useState(() => {
+    const savedData = localStorage.getItem("reservationData");
+    return savedData
+      ? JSON.parse(savedData)
+      : {
+          scheduleId: "",
+          trainId: "",
+          departureStationId: "",
+          fromHaltId: "",
+          toHaltId: "",
+          arrivalStationId: "",
+          departureDate: "",
+          pax: 0,
+          selectedSeats: [],
+          selectedClassId: "",
+          departureTime: "",
+          arrivalTime: "",
+          email: "",
+        };
+  });
 
-	return (
-		<ReservationContext.Provider
-			value={{ reservationData, setReservationData }}
-		>
-			{children}
-		</ReservationContext.Provider>
-	);
+  useEffect(() => {
+    localStorage.setItem("reservationData", JSON.stringify(reservationData));
+  }, [reservationData]);
+
+  return (
+    <ReservationContext.Provider
+      value={{ reservationData, setReservationData }}
+    >
+      {children}
+    </ReservationContext.Provider>
+  );
 };
 
-// ReservationProvider.propTypes = {
-// 	children: PropTypes.node.isRequired,
-// };
+ReservationProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
