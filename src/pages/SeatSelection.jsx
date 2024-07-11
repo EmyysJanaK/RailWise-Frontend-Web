5e2333af46825256ae72a4a8341f87f63907e959
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-07-11 17:03:54 +0530

    make seat selection component as a page

--- src/pages/SeatSelection.jsx
+++ src/pages/SeatSelection.jsx
@@ -1,103 +1,188 @@
-import { useContext, useState, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import axios from "axios";
+
 import { ReservationContext } from "../context/ReservationContext";
-import { z } from "zod";
-import SeatSelection from "../components/SeatSelection";
+import { UserContext } from "../context/UserContext";
+import Wagon from "../components/Wagon";
+import SeatSelectionDispaly from "../components/SeatSelectionDisplay";
 
-const emailSchema = z.string().email();
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, background: "red", borderRadius: "50%" }}
+      onClick={onClick}
+    />
+  );
+}
 
-const PassengerDetails = () => {
-  const { reservationData, setReservationData } = useContext(ReservationContext);
+const SeatSelectionPage = () => {
+  const { reservationData, setReservationData } =
+    useContext(ReservationContext);
   const navigate = useNavigate();
-  const { departureStationId, arrivalStationId, departureDate, selectedClassId } = reservationData;
-
+  const {
+    scheduleId,
+    trainId,
+    fromHaltId,
+    toHaltId,
+    departureDate,
+    selectedClassId,
+    pax,
+  } = reservationData;
+  const userId = useContext(UserContext)?.user?._id;
+  const [wagonsData, setWagonsData] = useState([]);
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const [disableSlider, setDisableSlider] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
-  const [isFormValid, setIsFormValid] = useState(false);
 
-  const validateEmail = (emailaddr) => {
-    try {
-      emailSchema.parse(emailaddr);
-      setError("");
-      return true;
-    } catch {
-      setError("Invalid email address");
-      return false;
-    }
-  };
-
-  const validateForm = () => {
-    if (!email) {
-      setError("Email is required");
-      return false;
-    }
-    if (!validateEmail(email)) {
-      return false;
-    }
-    setError("");
-    return true;
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <SamplePrevArrow />,
+    nextArrow: <SamplePrevArrow />,
   };
 
   useEffect(() => {
-    setIsFormValid(validateForm());
-  }, [email]);
+    const getSeats = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("/api/schedules/wagonsOfClass", {
+          params: {
+            scheduleId,
+            trainId,
+            fromHaltId,
+            toHaltId,
+            date: departureDate,
+            requestedClassId: selectedClassId,
+          },
+        });
+        console.log("response", response.data);
+        setWagonsData(response.data.requestedClassWagons);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setError("Failed to load seat data");
+        setLoading(false);
+      }
+    };
 
-  const handleBack = () => {
-    navigate(-1);
+    getSeats();
+  }, [fromHaltId, toHaltId, scheduleId, departureDate, setReservationData]);
+
+  const handleSeatClick = (seat, wagonNumber) => {
+    const isAvailable = selectedSeats.find(
+      (selectedSeat) => selectedSeat._id === seat._id
+    );
+    if (isAvailable) {
+      setSelectedSeats(
+        selectedSeats.filter((selectedSeat) => selectedSeat._id !== seat._id)
+      );
+    } else {
+      if (selectedSeats.length >= pax - 1) {  
+        setDisableSlider(true);
+      } else {
+        setDisableSlider(false);
+      }
+      setSelectedSeats([
+        ...selectedSeats,
+        { _id: seat._id, name: seat.name, wagonNumber },
+      ]);
+    }
   };
 
-  const handleSubmit = () => {
-    if (isFormValid) {
-      setReservationData((prevData) => ({
-        ...prevData,
-        email,
-      }));
-      setSuccess("Details submitted successfully!");
-      navigate("/reservationSummary");
+  const handleReset = () => {
+    setSelectedSeats([]);
+    setDisableSlider(false);
+  };
+
+  const handleProceed = async (selectedSeats) => {
+    setReservationData({
+      ...reservationData,
+      selectedSeats,
+    });
+    setLoading(true);
+    try {
+      const response = await axios.post("/api/bookings/createPendingBooking", {
+        scheduleId,
+        date: departureDate,
+        fromHaltId,
+        toHaltId,
+        selectedSeatIds: selectedSeats.map((seat) => seat._id),
+        selectedClassId,
+        userId,
+      });
+      setLoading(false);
+      console.log("response", response.data);
+      navigate("/payment-gateway", { state: { bookingId: response.data.bookingId, expireTime: response.data.expireTime } });
+    } catch (error) {
+      console.error(error);
+      setError("Failed to proceed to payment");
+      setLoading(false);
     }
   };
 
   return (
-    <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-      <h1 className="text-3xl font-extrabold text-gray-900 mb-8 text-center">
+    <>
+      <h1 className="mb-8 text-3xl font-extrabold text-center text-gray-900">
         Seat Selection and Email
       </h1>
-      <SeatSelection />
-      <div className="bg-white shadow-md rounded-lg p-6 mb-6">
-        <h2 className="text-2xl font-bold mb-4">Enter Email for Ticket</h2>
+      <div className="w-[500px] mx-auto mt-8">
+        <Slider {...settings}>
+          {wagonsData.map((wagonData) => (
+            <div key={wagonData._id} className="p-10 bg-red-600">
+              <Wagon
+                wagonNumber={wagonData.wagonNumber}
+                seats={wagonData.seats}
+                bookedSeats={wagonData.alreadyBookedSeats}
+                handleSeatClick={handleSeatClick}
+                selectedSeats={selectedSeats}
+                disableSlider={disableSlider}
+              />
+            </div>
+          ))}
+        </Slider>
+        <div className="w-1/2 mx-auto mt-8">
+          <SeatSelectionDispaly
+            selectedSeats={selectedSeats}
+            handleReset={handleReset}
+          ></SeatSelectionDispaly>
+        </div>
+        <button
+          className={`w-1/2 p-2 mx-auto mt-8 text-white rounded-lg ${
+            disableSlider
+              ? "bg-blue-500 cursor-pointer"
+              : "bg-gray-400 cursor-not-allowed"
+          }`}
+          onClick={() => handleProceed(selectedSeats)}
+          disabled={selectedSeats.length < pax}
+        >
+          Proceed to payment
+        </button>
+        <div className="p-6 mb-6 bg-white rounded-lg shadow-md">
+        <h2 className="mb-4 text-2xl font-bold">Enter Email for Ticket</h2>
         <div className="mb-4">
           <input
             type="email"
-            className="form-input mt-1 p-2 w-full border border-gray-300 rounded-md"
+            className="w-full p-2 mt-1 border border-gray-300 rounded-md form-input"
             placeholder="Email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            onBlur={(e) => validateEmail(e.target.value)}
           />
         </div>
-        {error && <p className="text-red-500">{error}</p>}
-        {success && <p className="text-green-500">{success}</p>}
       </div>
-      <div className="flex justify-between">
-        <button
-          onClick={handleBack}
-          className="bg-gray-300 text-gray-700 px-4 py-2 rounded-full hover:bg-gray-400 transition duration-300"
-        >
-          Back
-        </button>
-        <button
-          onClick={handleSubmit}
-          className={`bg-purple-500 text-white px-4 py-2 rounded-full hover:bg-purple-700 transition duration-300 ${
-            !isFormValid ? "opacity-50 cursor-not-allowed" : ""
-          }`}
-          disabled={!isFormValid}
-        >
-          Next
-        </button>
       </div>
-    </div>
+    </>
   );
 };
 
-export default PassengerDetails;
+export default SeatSelectionPage;
