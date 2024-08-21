927b34bbce1c024e156db82afd6f49516bce186b
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-08-21 23:29:10 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,39 +29,6 @@ const Login = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div className="relative isolate overflow-hidden bg-gray-700 py-24 sm:py-32">
-        
-        <div
-          aria-hidden="true"
-          className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
-        >
-        <div
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-            className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#75113a] to-[#9490e2] opacity-60"
-        />
-      </div>
-      <div className="relative z-10 w-full max-w-md mx-auto bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
-            RAILWISE LOGIN
-          </h2>
-        <div className="flex justify-center mb-8">
-          <img
-            src={trainImage}
-            alt="Railwise Logo"
-            className="w-40 h-40"
-          />
-        </div>
-        
-        {error && <div className="mb-4 text-red-600">{error}</div>}
-        <form onSubmit={handleLogin}>
-          
-          <TextInput label="Username" {...username} />
-          <PasswordInput label="Password" {...password} />
-=======
     <div className="relative z-10 w-full max-w-md p-8 mx-auto my-12 bg-white rounded-lg shadow-lg">
       {/* <Breadcrumbs title="Login" prevLocation={prevLocation} /> */}
       <div className="flex justify-center mb-6">
@@ -74,7 +41,6 @@ const Login = () => {
       <form onSubmit={handleLogin}>
         <TextInput label="Username or Email" {...username} />
         <PasswordInput label="Password" {...password} />
->>>>>>> a8740e7dfa53d714ab18e78e2073d39512b2b675
 
         <div className="mb-4">
           <Link to="/ForgotPassword" className="text-blue-500">
