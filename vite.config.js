159343fe76b77eaf574818185df1da0da3909aaf
supunnadeera <129364689+supunnadeera@users.noreply.github.com>
2024-09-22 11:30:53 +0530

    added the host to env

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: "http://192.168.8.159:3000",
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: `http://${env.VITE_HOST}:3000`,
+          secure: false,
+        },
       },
-    }
-  },
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/setupTests.js',  // Add this if you want global setup
-  },
-})
+    },
+    plugins: [react()],
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './src/setupTests.js',
+    },
+  };
+});
