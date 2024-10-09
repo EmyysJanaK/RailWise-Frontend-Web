29fcde375ab110950920cccbd0564e65b90a202b
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-10-09 13:34:16 +0530

    Update Success.jsx

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -25,9 +25,9 @@ const Success = () => {
     {state && state.bookingId &&(
     <div className="flex items-center justify-center min-h-screen bg-green-100">
       <div className="flex flex-col items-center justify-center p-8 text-center bg-white rounded-lg shadow-lg">
-        <FcApproval className="text-green-600  text-9xl" />
+        <FcApproval className="text-green-600 text-9xl" />
         <h1 className="mb-4 text-3xl font-bold text-green-600 ">Payment Successful</h1>
-        <p className="text-lg text-gray-700">Your payment was successful! You will be redirected to Home shortly.</p>
+        <p className="text-lg text-gray-700">Payment successful! Your e-tickets will be sent to the email you provided. You will be redirected to the homepage shortly.</p>
       </div>
     </div>
     )}
