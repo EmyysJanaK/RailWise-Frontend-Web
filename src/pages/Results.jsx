06593d41bad9f3aeb0296e1fb34e856ab542f8e4
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-10-09 06:56:26 +0530

    fix the filter

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -33,11 +33,25 @@ export default function Results() {
     navigate("/options");
   };
 
+  const filterTrainOptions = () => {
+    return trainOptions.filter(
+      (option) =>
+        selectedFeatures.length === 0 ||
+        selectedFeatures.includes(
+          option.scheduleType.charAt(0).toUpperCase() +
+            option.scheduleType.slice(1)
+        )
+    );
+  };
+
   return (
     <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div className="flex flex-col gap-6 md:flex-row lg:px-20 ml:6">
         <div className={`flex-1 w-full p-1 `}>
-          <Filter selectedFeatures={selectedFeatures} setSelectedFeatures={setSelectedFeatures} />
+          <Filter
+            selectedFeatures={selectedFeatures}
+            setSelectedFeatures={setSelectedFeatures}
+          />
         </div>
 
         <div className="flex-1 flex flex-col gap-4 ml-0 md:ml-8 lg:flex-[3]">
@@ -46,23 +60,23 @@ export default function Results() {
           ) : error ? (
             <div className="text-red-500">{error}</div>
           ) : (
-            trainOptions.length > 0 ? (
-              trainOptions.map((option) => (
-                (selectedFeatures.length == 0 ||selectedFeatures.includes(option.scheduleType.charAt(0).toUpperCase() + option.scheduleType.slice(1))) ? (
+            (() => {
+              const filteredTrainOptions = filterTrainOptions();
+
+              return filteredTrainOptions.length > 0 ? (
+                filteredTrainOptions.map((option) => (
                   <TrainOption
                     key={option.id}
                     option={option}
                     onClick={() => handleTrainOptionClick(option)}
                   />
-                ): (              <div className="ml-3 text-2xl font-bold text-gray-700 capitalize">
+                ))
+              ) : (
+                <div className="ml-3 text-2xl font-bold text-gray-700 capitalize">
                   No trains available
-                </div>)
-              ))
-            ) :(
-              <div className="ml-3 text-2xl font-bold text-gray-700 capitalize">
-            No trains available
-          </div>
-            )
+                </div>
+              );
+            })()
           )}
         </div>
       </div>
