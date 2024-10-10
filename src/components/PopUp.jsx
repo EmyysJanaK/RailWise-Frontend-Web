e66fc56afd69655bbec36871e051f97c3aa2279c
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-10-10 15:16:42 +0530

    fix the login pop up issue

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -7,20 +7,21 @@ const PopUp = ({ onClose, onLogin }) => {
       <div className="p-6 bg-white rounded-lg shadow-xl">
         <h2 className="mb-4 text-xl font-bold text-center">HELLO!</h2>
         <p className="mb-6 text-center">
-          As a registered user, you can view your booking history, cancel bookings, and receive exclusive offers!
+          As a registered user, you can view your booking history, cancel
+          bookings, and receive exclusive offers!
         </p>
         <div className="flex justify-center gap-4">
           <button
-            className="px-4 py-2 font-semibold text-white bg-purple-900 rounded-lg hover:bg-gray-700"
+            className="px-10 py-2 font-semibold text-white bg-purple-900 rounded-lg hover:bg-gray-700"
             onClick={onLogin}
           >
             Login
           </button>
           <button
-            className="px-4 py-2 font-semibold text-white bg-purple-900 rounded-lg hover:bg-gray-600"
+            className="px-4 py-2 font-semibold text-white bg-gray-700 rounded-lg hover:bg-gray-600"
             onClick={onClose}
           >
-            Close
+            Continue Without Login
           </button>
         </div>
       </div>
